67da0d264d00a76692728f488ef27103d74bdf7f
qiuguanglin <tmdyyzc$123>
2020-01-09 12:56:31 +0800

    add default port if not specified

--- WebApp.js
+++ WebApp.js
@@ -2,6 +2,10 @@
 const express = require('express');
 
 const App = express();
-const port = process.argv[2];
+let port = process.argv[2];
+if(!port){
+  port = 80;
+}
+
 App.use(express.static('public'))
   .listen(port, console.log(`web server stated at ${port}`));
